0a65a3a7852801bee5e4a44082e8b292a299df71
Benjamin Small <ben20262@aim.com>
2019-12-10 22:21:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line (array)
+  words = "The line is currently:"
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    counter = 1
+    array.each do |name|
+      words = words + " #{counter} #{name}"
+    end
+    puts words
+  end
+end
+
+def take_a_number (array, name)
+  puts "Welcome, #{name} you are number #{array.size + 1} in line."
+end
